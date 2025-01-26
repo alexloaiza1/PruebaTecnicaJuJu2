b530912ea56feae32788f24b07bb20103ae3f9a9
Alexander Loaiza <alexloaiza1@gmail.com>
2025-01-26 18:45:22 -0500

    Agrego controlador con todos los metodos crud para la gestion de libros

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -30,6 +30,70 @@ const createBook = async (req, res) => {
   }
 };
 
+const getAllBooks = async (req, res) => {
+  try {
+    const books = await Book.findAll();
+    res.status(200).json(books);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const updateBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { titulo, autor, anio, estado, userid } = req.body;
+
+    // Asegúrate de que 'anio' esté en el formato correcto
+
+    const [updated] = await Book.update(
+      {
+        Titulo: titulo,
+        Autor: autor,
+        AnioPublicacion: anio,
+        Estado: estado,
+        UsuarioCreacion: userid,
+      },
+      {
+        where: { LibroID: id },
+      }
+    );
+
+    if (!updated) {
+      return res.status(404).json({ error: "Libro no encontrado" });
+    }
+
+    const updatedBook = await Book.findOne({ where: { LibroID: id } });
+    res.status(200).json(updatedBook);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const deleteBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deleted = await Book.destroy({
+      where: { LibroID: id },
+    });
+
+    if (!deleted) {
+      return res.status(404).json({ error: "Libro no encontrado" });
+    }
+
+    res.status(200).json({ message: "Libro eliminado correctamente" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+};
+
 module.exports = {
   createBook,
+  updateBook,
+  getAllBooks,
+  deleteBook,
 };
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -4,9 +4,8 @@ const bookController = require("../controllers/bookController");
 
 // Definir rutas para los libros
 router.post("/books", bookController.createBook);
-//router.get("/books", bookController.getAllBooks);
-//router.get("/books/:id", bookController.getBookById);
-//router.put("/books/:id", bookController.updateBook);
-//router.delete("/books/:id", bookController.deleteBook);
+router.put("/books/:id", bookController.updateBook);
+router.get("/books", bookController.getAllBooks);
+router.delete("/books/:id", bookController.deleteBook);
 
 module.exports = router; // Asegúrate de exportar el router correctamente
